c73e5a3166934eadd3bb390aefd1f3ec163baeba
dean- <dean-@SHADOW-F7V6B1NP>
2018-06-05 23:26:19 +0200

    Finanzverwaltung

---
+++ Propra/src/Data/Finanzverwaltung.java
@@ -0,0 +1,31 @@
+package Data;
+
+public class Finanzverwaltung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	// Rechnung 
+	
+	public static void addBill() {
+		
+	}
+	
+	public static void alterBill() {
+		
+	}
+
+	public static void deleteBill() {
+		
+	}
+	public static void addOrderToBill() {
+		
+	}
+	
+	
+	
+	
+}
