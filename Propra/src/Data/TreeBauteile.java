1efaf58ac316788b32ff1be6c9ef81d121b3fc88
dean- <dean-@SHADOW-F7V6B1NP>
2018-06-22 12:17:59 +0200

    Baumstruktur Bauteile

---
+++ Propra/src/Data/TreeBauteile.java
@@ -0,0 +1,33 @@
+package Data;
+
+public class TreeBauteile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void addTKat() {
+		
+	}
+	public static void alterTKat() {
+		
+	}
+	public static void delteTKat() {
+		
+	}
+	
+	//Bauteil
+	public static void addTBauteil() {
+		
+	}
+	public static void alterTBauteil() {
+		
+	}
+	public static void delteTBauteil() {
+		
+	}
+	
+
+	
+}
