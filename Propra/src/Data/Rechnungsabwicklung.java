2a174c9f5bbe14bb7f2fab02842ceee15cea0ed2
dean- <dean-@SHADOW-F7V6B1NP>
2018-06-28 21:18:10 +0200

    Gast-Account, Rechnungen Neu

---
+++ Propra/src/Data/Rechnungsabwicklung.java
@@ -0,0 +1,41 @@
+package Data;
+
+public class Rechnungsabwicklung {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	//BRechnung
+	
+	public static void createBRechnung() {
+		
+	}
+	
+	public static void AddTBauteilToBRechnung() {
+		
+	}
+	
+	public static void alterBRechnung() {
+		
+	}
+	
+	public static void delteBRechnung() {
+		
+	}
+	
+	// ARechnung
+	
+	public static void createARechnung() {
+		
+	}
+
+	public static void alterARechnung() {
+		
+	}
+	
+	public static void delteARechnung() {
+		
+	}
+}
